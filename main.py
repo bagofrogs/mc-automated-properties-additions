0aa07307871a470f0e95a5f08870b3778071f05c
Allexio <x.allexio@gmail.com>
2024-04-18 09:32:17 +0200

    added descriptions to generated .properties

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from json import load
-from os import listdir
-
+from os import listdir # needed to list the .json files in the folder
+from json import load # needed to load the .json files
 
 def loader() -> dict:
     """Generates dictionary with all the information from local json files
@@ -16,13 +15,34 @@ def loader() -> dict:
         data_dict[filename] = load(open("./input-jsons/" + file)) # load json from file into a python dict
     return data_dict
 
+def description_obtainer(vanilla_data: dict) -> dict:
+    """Takes a dictionary of vanilla values and returns a dict
+
+    Args:
+        vanilla_data (dict): the vanilla json in python dictionary format
+
+    Returns:
+        description_data (dict): a dictionary where the format is as follows:
+        "block": {
+            12345: "blabla",
+            67890: "blublu"
+        },
+        "item": {etc.}
+    """
+    description_data = {}
+    for block_type in vanilla_data: # block/item/entity
+        description_data[block_type] = {} # instantiate the different dictionaries per block type
+        for object in vanilla_data[block_type]:
+            description_data[block_type][object["block_id"]] = object["description"]
+    return description_data
+
 def merger(data_dict: dict) -> dict:
     """takes a dictionary with multiple keys that correspond to different files and merges them to obtain a single dict with blocks and items directly
     
     Args:
         data_dict (dict): a dictionary where keys are derived from filenames:
             "chipped": {
-                "blocks": [
+                "block": [
                     "description": "blabla",
                     "block_id": 12345,
                     "affected_blocks": "mod:block",
@@ -49,8 +69,7 @@ def merger(data_dict: dict) -> dict:
                 merged_dict[object_type][object["block_id"]].extend(full_object_id_list)
     return merged_dict
 
-
-def writer(merged_dict : dict):
+def writer(merged_dict : dict, description_data : dict):
     """Takes a merged dict and writes to local x.properties files
 
     Args:
@@ -60,11 +79,18 @@ def writer(merged_dict : dict):
     for object_type in merged_dict: # block or item    
         output_string = ""
         for block_id in merged_dict[object_type]:
-            output_string+= "\n" + object_type + "." + str(block_id) + " = " + " ".join(merged_dict[object_type][block_id])
+            output_string+= "\n# " + description_data[object_type][block_id] # add the description on top
+            output_string+= "\n" + object_type + "." + str(block_id) + " = " + " ".join(merged_dict[object_type][block_id]) # add the block ID and affected blocks
+        
+        
         file = open(mode="a", file=object_type+".properties")    
         file.write(output_string)
         file.close()
 
-raw_dict = loader()
-merged_dict = merger(raw_dict)
-writer(merged_dict)
+
+
+raw_data = loader()
+description_data = description_obtainer(raw_data["vanilla"])
+merged_dict = merger(raw_data)
+
+writer(merged_dict, description_data)
